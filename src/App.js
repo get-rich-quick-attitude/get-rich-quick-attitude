7595380c47ba031853b19d2ea23326378283c258
rhsok823 <rhsok823@gmail.com>
2023-01-08 23:06:38 +0900

    UI_add_1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <div>로고</div>
+        <div>언어</div>
+        <div>안녕</div>
+      </header>
+      <div id="container">
+        <div id="timerbox">hi</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
